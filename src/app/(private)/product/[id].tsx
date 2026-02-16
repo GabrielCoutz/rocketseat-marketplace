cc33dce9b25c2d35932f2e29a1ab7e4510f99922
Gabrielcoutz <gabriel.coutinho.cassiano@gmail.com>
2026-02-16 11:33:21 -0300

    feat: Add support for feedback bottom sheet in product view

--- src/app/(private)/product/[id].tsx
+++ src/app/(private)/product/[id].tsx
@@ -3,8 +3,14 @@ import { useProductViewModel } from "../../../viewModels/Product/useProduct.view
 import { ProductView } from "../../../viewModels/Product/Product.view";
 
 export default function Product() {
-  const { id } = useLocalSearchParams<{ id: string }>();
-  const viewModel = useProductViewModel(Number(id));
+  const { id, openFeedbackBottomsheet } = useLocalSearchParams<{
+    id: string;
+    openFeedbackBottomsheet?: string;
+  }>();
+  const viewModel = useProductViewModel(
+    Number(id),
+    Boolean(openFeedbackBottomsheet),
+  );
 
   return <ProductView {...viewModel} />;
 }
